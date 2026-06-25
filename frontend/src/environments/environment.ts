853e8801c81dbce06d16189c30f7cdfa2a6de5aa
Talles Oliveira <tallesoliveirags@gmail.com>
2026-06-25 19:42:16 -0300

    Ajude de environment

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   apiBaseUrl: 'http://localhost:3000',
-  supabaseUrl: 'https://example.supabase.co',
-  supabaseAnonKey: 'public-anon-key',
+  supabaseUrl: 'https://ouowadvxdddzegcjviog.supabase.co',
+  supabaseAnonKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im91b3dhZHZ4ZGRkemVnY2p2aW9nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI0MTUxMDAsImV4cCI6MjA5Nzk5MTEwMH0.H8k9T5fQJucVeCzGO7fCWhk8iQizm3S2cqAo1Pk3I-0',
 };
